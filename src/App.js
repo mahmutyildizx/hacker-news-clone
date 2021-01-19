037f9175cde5eb0baca0db139d9af4c9798f5d17
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-19 23:39:59 +0300

    add api call

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getStoryIds } from "./services/hackernewsApi";
 
-export const App = () => <p>Hello</p>;
+export const App = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    console.log(getStoryIds());
+  }, []);
+
+  return <p>{storyIds}</p>;
+};
