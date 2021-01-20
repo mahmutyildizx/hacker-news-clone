345f89bb52f981205e02e5192dca864366c6522e
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-21 00:12:51 +0300

    add selectfields utils for api

--- src/services/hackernewsApi.js
+++ src/services/hackernewsApi.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {selectFields} from '../utils/selectFields';
 
 export const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 export const newStoriesUrl = `${baseUrl}newstories.json`;
@@ -7,7 +8,7 @@ export const storyUrl = `${baseUrl}item/`;
 export const getStory = async (storyId) => {
   const result = await axios
     .get(`${storyUrl + storyId}.json`)
-    .then(({ data }) => data);
+    .then(({ data }) => data && selectFields(data));
 
   return result;
 };
