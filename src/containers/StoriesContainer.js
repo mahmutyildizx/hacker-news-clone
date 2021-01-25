f56405fa8fa14731c9ee6fcb9d53c6ffc0a512dc
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-25 22:11:55 +0300

    add global css and main style

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { getStoryIds } from "../services/hackernewsApi";
 import { Story } from "../components/Story";
+import {
+  GlobalStyle,
+  StoriesContainerWrapper,
+} from "../styles/StoriesContainerStyles";
 
 export const StoriesContainer = () => {
   const [storyIds, setStoryIds] = useState([]);
@@ -11,10 +15,13 @@ export const StoriesContainer = () => {
 
   return (
     <>
-      <h1>Hacker News Stories</h1>
-      {storyIds.map((storyId) => (
-        <Story storyId={storyId} key={storyId} />
-      ))}
+      <GlobalStyle />
+      <StoriesContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story storyId={storyId} key={storyId} />
+        ))}
+      </StoriesContainerWrapper>
     </>
   );
 };
