ca439dedd097c5b69ad323072df0fcd15d71ce5f
mahmuttyildiz <mahmutt.yildiz@outlook.com>
2021-01-26 00:16:49 +0300

    add infinite scroll

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import { getStoryIds } from "../services/hackernewsApi";
 import { Story } from "../components/Story";
 import {
   GlobalStyle,
   StoriesContainerWrapper,
 } from "../styles/StoriesContainerStyles";
+import { useInfiteScroll } from "../hooks/useInfiteScroll";
 
 export const StoriesContainer = () => {
+  const {count} = useInfiteScroll;
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
@@ -18,7 +20,7 @@ export const StoriesContainer = () => {
       <GlobalStyle />
       <StoriesContainerWrapper data-test-id="stories-container">
         <h1>Hacker News Stories</h1>
-        {storyIds.map((storyId) => (
+        {storyIds.slice(0, count).map((storyId) => (
           <Story storyId={storyId} key={storyId} />
         ))}
       </StoriesContainerWrapper>
---
+++ src/hooks/useInfiteScroll.js
@@ -0,0 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { STORY_INCREMENT, MAX_STORIES } from "../constants";
+
+export const useInfiteScroll = () => {
+  const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(STORY_INCREMENT);
+
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      loading
+    ) {
+      return false;
+    }
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    if (!loading) return;
+    if (count + STORY_INCREMENT >= MAX_STORIES) {
+      setCount(MAX_STORIES);
+    } else {
+      setCount(count + STORY_INCREMENT);
+    }
+    setLoading(false);
+  }, [loading]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return { count };
+};
